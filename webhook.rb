e22141b57ab212c0ccf43f5e4970f08e355fd24c
krujos <jkruck@pivotal.io>
2014-12-23 14:18:59 -0600

    plausable implementation

--- webhook.rb
+++ webhook.rb
@@ -4,6 +4,7 @@ require 'json'
 
 github_secret = ENV['GITHUB_SECRET']
 github_access_token = ENV['GITHUB_ACCESS_TOKEN']
+github_collobarator = ENV['GITHUB_COLLABORATOR']
 
 def die(var)
   abort "#{var} is not set. Set #{var}! Hint: use 'cf env' to set environment variables."
@@ -17,18 +18,22 @@ if github_access_token.nil?
   die('GITHUB_ACCESS_TOKEN')
 end
 
-
+if github_collobarator.nil?
+  die('GITHUB_COLLABORATOR')
+end
 
 get '/' do
   'Hello World'
 end
 
 post '/' do
-  the_json = JSON.parse request.body.read
-  repo = the_json['repository']['full_name']
-  puts repo
+  json = JSON.parse request.body.read
+  repo = json['repository']['id']
+  repo_name = json['repository']['full_name']
   client = Octokit::Client.new(:access_token => github_access_token)
-  client.repository(repo)
-
+  unless client.add_collaborator repo, github_collobarator, 'write'
+    logger.error "Failed to add #{github_collobarator} to #{repo_name}"
+  end
+  logger.info "Added #{github_collobarator} to #{repo_name}"
 end
 
